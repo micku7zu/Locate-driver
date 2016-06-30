cd0c6399845de225becb2e8165be23f34829b795
Sergiu Șandor <micku7zu@gmail.com>
2016-07-01 01:12:37 +0300

    Add logo, and MaterialDrawer

--- app/src/main/java/com/micutu/locatedriver/MainActivity.java
+++ app/src/main/java/com/micutu/locatedriver/MainActivity.java
@@ -2,12 +2,35 @@ package com.micutu.locatedriver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Drawer drawer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupDrawerAndToolbar();
+    }
+
+    protected void setupDrawerAndToolbar() {
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withHeader(R.layout.header)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Test").withIdentifier(1)
+                )
+                .build();
     }
 }
