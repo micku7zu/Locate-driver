5c3efe18a4745f9ae85cc7a55cbdcb10cec6c38e
Sergiu Șandor <micku7zu@gmail.com>
2016-07-01 15:09:47 +0300

    Implement LDCard and basic form

--- app/src/main/java/com/micutu/locatedriver/MainActivity.java
+++ app/src/main/java/com/micutu/locatedriver/MainActivity.java
@@ -1,8 +1,18 @@
 package com.micutu.locatedriver;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.micutu.locatedriver.Views.LDCard;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupToolbar();
+
+        Button startButton = (Button) this.findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View current = getCurrentFocus();
+                if (current != null) current.clearFocus();
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        });
     }
 
     protected void setupToolbar() {
---
+++ app/src/main/java/com/micutu/locatedriver/Views/LDCard.java
@@ -0,0 +1,62 @@
+package com.micutu.locatedriver.Views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.micutu.locatedriver.R;
+
+
+public class LDCard extends CardView {
+        private TypedArray attributes = null;
+        private LinearLayout linearLayout = null;
+        private TextView title = null;
+
+        public LDCard(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            attributes = getContext().obtainStyledAttributes(attrs, R.styleable.LDCard);
+        }
+
+        @Override
+        protected void onFinishInflate() {
+
+            View[] children = this.detachChildren();
+            inflate(getContext(), R.layout.ldcard, this);
+
+            linearLayout = (LinearLayout) this.findViewById(R.id.linear_layout);
+            title = (TextView) this.findViewById(R.id.title);
+            title.setText(attributes.getString(R.styleable.LDCard_title));
+
+
+            for (int i = 0; i < children.length; i++) {
+                this.addView(children[i]);
+            }
+
+            attributes.recycle();
+            super.onFinishInflate();
+        }
+
+        private View[] detachChildren() {
+            int count = this.getChildCount();
+            View[] views = new View[count];
+            for (int i = 0; i < count; i++) {
+                views[i] = this.getChildAt(i);
+            }
+
+            this.detachAllViewsFromParent();
+
+            return views;
+        }
+
+        @Override
+        public void addView(View view) {
+            if (linearLayout != null) {
+                linearLayout.addView(view);
+            }
+        }
+}
