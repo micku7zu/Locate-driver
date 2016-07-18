b8ea5ff6907309275e1d8830c68343e25b98ca2a
micku7zu <micku7zu@gmail.com>
2016-07-18 21:44:36 +0300

    Fix height of settings card on some phones.

--- app/src/main/java/com/micutu/locatedriver/Fragments/SettingsCardFragment.java
+++ app/src/main/java/com/micutu/locatedriver/Fragments/SettingsCardFragment.java
@@ -3,11 +3,15 @@ package com.micutu.locatedriver.Fragments;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Adapter;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.micutu.locatedriver.R;
 
@@ -25,27 +29,41 @@ public class SettingsCardFragment extends PreferenceFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        int width = Resources.getSystem().getDisplayMetrics().widthPixels;
-
-        /* calculate preferences height using measure and set it to parent */
-        if (getView() != null) {
-            ListView listView = (ListView) getView().findViewById(android.R.id.list);
-            Adapter adapter = listView.getAdapter();
-            if (adapter != null) {
-                int height = 0;
-                for (int i = 0; i < adapter.getCount(); i++) {
-                    View item = adapter.getView(i, null, listView);
-                    item.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY), 0);
-                    height += item.getMeasuredHeight();
-                }
-                FrameLayout frame = (FrameLayout) getActivity().findViewById(R.id.settings_preferences_framelayout);
-                ViewGroup.LayoutParams param = frame.getLayoutParams();
-                param.height = height + (listView.getDividerHeight() * (adapter.getCount()));
-                frame.setLayoutParams(param);
-            }
+        if (getView() == null) {
+            return;
         }
 
+        final ListView listView = (ListView) getView().findViewById(android.R.id.list);
+        final View parent = (View) listView.getParent();
 
+        ViewTreeObserver viewTreeObserver = parent.getViewTreeObserver();
+        if (viewTreeObserver.isAlive() == false) {
+            return;
+        }
+
+        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                parent.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                int width = parent.getWidth() - listView.getPaddingLeft() - listView.getPaddingRight();
+
+                Adapter adapter = listView.getAdapter();
+                if (adapter != null) {
+                    int height = 0;
+                    for (int i = 0; i < adapter.getCount(); i++) {
+                        View item = adapter.getView(i, null, listView);
+                        item.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY), 0);
+                        height += item.getMeasuredHeight();
+                    }
+
+                    FrameLayout frame = (FrameLayout) getActivity().findViewById(R.id.settings_preferences_framelayout);
+                    ViewGroup.LayoutParams param = frame.getLayoutParams();
+                    param.height = height + (listView.getDividerHeight() * (adapter.getCount()));
+                    frame.setLayoutParams(param);
+                }
+            }
+        });
 
     }
 }
