4f5d95c81b7e0f3357f184cedbf6c61f2b62b51d
micku7zu <micku7zu@gmail.com>
2016-07-19 00:29:03 +0300

    Improve location get

--- app/src/main/java/com/micutu/locatedriver/Services/SmsSenderService.java
+++ app/src/main/java/com/micutu/locatedriver/Services/SmsSenderService.java
@@ -6,13 +6,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.location.Location;
+import android.location.LocationManager;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.telephony.SmsManager;
 
 import com.google.gson.Gson;
 import com.micutu.locatedriver.Model.LDPlace;
 import com.micutu.locatedriver.R;
-import com.micutu.locatedriver.Utilities.LocationGetter;
 import com.micutu.locatedriver.Utilities.Network;
 
 import org.json.JSONException;
@@ -20,8 +21,15 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class SmsSenderService extends IntentService implements LocationGetter.OnLocationGetListener {
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
+import io.nlopez.smartlocation.location.config.LocationParams;
+import io.nlopez.smartlocation.location.providers.LocationGooglePlayServicesWithFallbackProvider;
+
+public class SmsSenderService extends IntentService implements OnLocationUpdatedListener {
     private final static String TAG = SmsSenderService.class.getSimpleName();
+
+    private final static int LOCATION_REQUEST_MAX_WAIT_TIME = 60;
 
     private Resources r = null;
     private Context context = null;
@@ -34,6 +42,9 @@ public class SmsSenderService extends IntentService implements LocationGetter.On
     private boolean networkSms = false;
 
     private boolean alreadySentFlag = false;
+
+    private Location bestLocation = null;
+    private long startTime = 0;
 
     public SmsSenderService() {
         super("SmsSenderService");
@@ -63,37 +74,93 @@ public class SmsSenderService extends IntentService implements LocationGetter.On
             this.sendAcknowledgeMessage(phoneNumber);
         }
 
-        LocationGetter locationGetter = new LocationGetter(context, this); //listener implemented below
+        //set bestLocation to null and start time
+        startTime = System.currentTimeMillis() / 1000;
+        bestLocation = null;
+
+        SmartLocation.with(context).location(new LocationGooglePlayServicesWithFallbackProvider(context))
+                .config(LocationParams.NAVIGATION)
+                .start(this);
+
+    }
+
+    public static boolean isLocationFused(Location location) {
+        return !location.hasAltitude() || !location.hasSpeed() || location.getAltitude() == 0;
     }
 
     @Override
-    public void onLocationGet(Location location) {
-        //Log.d(TAG, "onLocationGet " + location);
+    public void onLocationUpdated(Location location) {
+        //Log.d(TAG, "LOCATION UPDATE");
 
-        if (alreadySentFlag) { //for protection
-            //Log.d(TAG, "ALREADY SENT, CEVA SE INTAMPLA!!!");
-            return;
+        long currentTime = System.currentTimeMillis() / 1000;
+
+        //Log.d(TAG, "Start time: " + startTime + ", Current time: " + currentTime);
+        //Log.d(TAG, "Difference: " + (currentTime - startTime));
+
+        if (currentTime - startTime < this.LOCATION_REQUEST_MAX_WAIT_TIME) {
+            //Log.d(TAG, "NOT EXPIRED YET. CHECK");
+
+            if (bestLocation == null) {
+                bestLocation = location;
+            }
+
+            //still null? check again
+            if (bestLocation == null) {
+                //Log.d(TAG, "BEST LOCATION STILL NULL, CHECK MORE");
+                return;
+            }
+
+            //Log.d(TAG, bestLocation.toString());
+            //Log.d(TAG, location.toString());
+
+            //Log.d(TAG, "HAS ALTITUDE:" + location.hasAltitude());
+            //Log.d(TAG, "HAS SPEED: " + location.hasSpeed());
+            //Log.d(TAG, "LOCATION PROVIDER: " + location.getProvider());
+
+
+            if (!bestLocation.getProvider().equals(LocationManager.GPS_PROVIDER) || bestLocation.getProvider().equals(location.getProvider())) {
+                //Log.d(TAG, "NOT GPS OR BOTH GPS!");
+                if (location.getAccuracy() < bestLocation.getAccuracy()) {
+                    //Log.d(TAG, "Update best location.");
+                    bestLocation = location;
+                }
+            }
+
+
+            if (this.isLocationFused(bestLocation)) {
+                //Log.d(TAG, "Location still fused.");
+                return;
+            }
+
+            if (bestLocation.getAccuracy() > 100) {
+                //Log.d(TAG, "Accuracy more than 100, check again.");
+                return;
+            }
         }
-        alreadySentFlag = true;
 
-        if (location == null) {
+
+        //stop the location
+        //Log.d(TAG, "STOP LOCATION BECAUSE TIME ELAPSED OR ACCURACY IS GOOD");
+        SmartLocation.with(context).location().stop();
+
+        if (bestLocation == null) {
             this.sendSMS(phoneNumber, r.getString(R.string.error_getting_location));
             return;
         }
 
         if (gpsSms) {
-            this.sendLocationMessage(phoneNumber, location);
+            this.sendLocationMessage(phoneNumber, bestLocation);
         }
 
         if (googleMapsSms) {
-            this.sendGoogleMapsMessage(phoneNumber, location);
+            this.sendGoogleMapsMessage(phoneNumber, bestLocation);
         }
 
         if (!networkSms) {
             return;
         }
 
-        this.sendNetworkMessage(phoneNumber, location, place, new OnNetworkMessageSentListener() {
+        this.sendNetworkMessage(phoneNumber, bestLocation, place, new OnNetworkMessageSentListener() {
             @Override
             public void onNetworkMessageSent() {
                 //Log.d(TAG, "on Network Message Sent");
@@ -130,17 +197,29 @@ public class SmsSenderService extends IntentService implements LocationGetter.On
         Resources r = context.getResources();
         String text = r.getString(R.string.acknowledgeMessage);
         text += " " + r.getString(R.string.network) + " " + this.booleanToString(Network.isNetworkAvailable(context));
-        text += ", " + r.getString(R.string.gps) + " " + LocationGetter.locationToString(context, LocationGetter.getLocationMode(context));
+        text += ", " + r.getString(R.string.gps) + " " + this.locationToString(context, this.getLocationMode(context));
         SmsSenderService.sendSMS(phoneNumber, text);
     }
 
     public void sendLocationMessage(String phoneNumber, Location location) {
         //Log.d(TAG, "sendLocationMessage()" + location.getAccuracy());
         Resources r = context.getResources();
-        String text = r.getString(R.string.accuracy) + " " + location.getAccuracy() + "m\n";
+        Boolean fused = isLocationFused(location);
+
+        String text = r.getString(fused ? R.string.approximate : R.string.accurate) + " location:\n";
+
+        text += r.getString(R.string.accuracy) + " " + location.getAccuracy() + "m\n";
         text += r.getString(R.string.latitude) + " " + location.getLatitude() + "\n";
         text += r.getString(R.string.longitude) + " " + location.getLongitude() + "\n";
-        text += r.getString(R.string.speed) + " " + location.getSpeed() + "KM/H";
+
+        if (location.hasSpeed()) {
+            text += r.getString(R.string.speed) + " " + location.getSpeed() + "KM/H\n";
+        }
+
+        if (location.hasAltitude() && location.getAltitude() != 0) {
+            text += r.getString(R.string.altitude) + " " + location.getAltitude() + "m";
+        }
+
         SmsSenderService.sendSMS(phoneNumber, text);
     }
 
@@ -150,6 +229,8 @@ public class SmsSenderService extends IntentService implements LocationGetter.On
         SmsSenderService.sendSMS(phoneNumber, text);
     }
 
+
+    /* NEEDS REFACTOR */
     public void sendNetworkMessage(final String phoneNumber, final Location location, final LDPlace place, final OnNetworkMessageSentListener onNetworkMessageSentListener) {
         //Log.d(TAG, "sendNetworkMessage() " + location.getAccuracy());
 
@@ -214,6 +295,29 @@ public class SmsSenderService extends IntentService implements LocationGetter.On
     public void onDestroy() {
         //Log.d(TAG, "onDestroy()");
         super.onDestroy();
+    }
+
+    public static int getLocationMode(Context context) {
+        try {
+            return Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+        } catch (Settings.SettingNotFoundException e) {
+            return -1;
+        }
+    }
+
+    public static String locationToString(Context context, int mode) {
+        switch (mode) {
+            case Settings.Secure.LOCATION_MODE_OFF:
+                return context.getResources().getString(R.string.location_mode_off);
+            case Settings.Secure.LOCATION_MODE_BATTERY_SAVING:
+                return context.getResources().getString(R.string.location_battery_saving);
+            case Settings.Secure.LOCATION_MODE_SENSORS_ONLY:
+                return context.getResources().getString(R.string.locateion_sensors_only);
+            case Settings.Secure.LOCATION_MODE_HIGH_ACCURACY:
+                return context.getResources().getString(R.string.location_high_accuracy);
+            default:
+                return "Error";
+        }
     }
 }
 
